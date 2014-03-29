833723d3fe0ddc8eed9e228667996ded992ff625
Antony <blokant@gmail.com>
2014-03-29 16:41:05 +0400

    setters and getters done for gtaskList

--- gtasklist.h
+++ gtasklist.h
@@ -2,17 +2,29 @@
 #define GTASKLIST_H
 
 #include <QObject>
-
+#include <QDateTime>
 class gTaskList : public QObject
 {
     Q_OBJECT
 public:
     explicit gTaskList(QObject *parent = 0);
+    void setTitle(const QString& newTitle){title = newTitle;}
+    QString getTitle(){return title;}
+    QString getId(){return id;}
+    QString getEtag(){return etag;}
+    QString getSelfLing(){return selfLink;}
+    QDateTime getUpdated(){return updated;}
 
 signals:
 
 public slots:
 
+private:
+    QString id;
+    QString title;
+    QString etag;
+    QString selfLink;
+    QDateTime updated;
 
 };
 
